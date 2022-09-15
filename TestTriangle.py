5eaaba4187ebea808c38a6dd1bf644a0c8ac19de
PeaceM1 <pmagomero423@gmail.com>
2022-09-14 21:09:17 -0400

    code works, but testcase won't

--- TestTriangle.py
+++ TestTriangle.py
@@ -2,14 +2,29 @@ import unittest
 
 
 def classify_triangle(a, b, c):
-    result = ""
     if a == b and a == c and b == c:
-        return 'Equilateral triangle'
+        print('Equilateral triangle')
     elif a == b or a == c or b == c:
-        return 'Isosceles triangle'
+        print('Isosceles triangle')
     else:
-        return 'Scalene triangle'
+        print('Scalene triangle')
+
     if (a ** 2 + b ** 2 == c ** 2) or (a ** 2 + c ** 2 == b ** 2) or (b ** 2 + c ** 2 == a ** 2):
-        return 'Right triangle'
+        print('Right triangle')
     else:
-        return 'Not a right triangle'
+        print('Not a right triangle')
+
+
+class TestTriangles(unittest.TestCase):
+    def equal_sides(self):
+        self.assertEqual(classify_triangle(2, 2, 2))
+
+
+print('Enter 3 side lengths')
+a = int(input())
+b = int(input())
+c = int(input())
+classify_triangle(a, b, c)
+
+if __name__ == '__main__':
+    unittest.main()
